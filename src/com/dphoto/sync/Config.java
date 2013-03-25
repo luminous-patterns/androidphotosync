5745c2e70634086d4087763a5aeaf7bd7578f4cf
Navjot Singh <weavebytes@gmail.com>
2013-03-25 20:27:42 +0530

    adding config file to store app configs

---
+++ src/com/dphoto/sync/Config.java
@@ -0,0 +1,25 @@
+package com.dphoto.sync;
+
+public class Config {
+	public static final int    LOC_SLEEP       = 1000;
+	
+	// DPHOTO API Application credentials
+
+	public static final String GALLERY_ID      = "testweavebytes.dphoto.com";
+	//public static final String GALLERY_ID      = "expweavebytes.dphoto.com";
+	public static final String ALBUM_ID        = "zgmwnm"; //for testweavebytes
+	//public static final String ALBUM_ID        = "mda3nd";   //for expweavebytes
+	
+	public static final String API_URL         = "https://api.dphoto.com/";
+	public static final String API             = "tag/getFiles";
+	//public static final String API           = "file/addTag";
+	//public static final String API           = "file/getInfo";
+	//public static final String API           = "album/getContents";
+	
+	public static final String APP_KEY     	   = "faa5307c52a04612d40bc27ea42c9a83";
+	public static final String APP_SECRET      = "cce6c3f34af56905c6935288355164d4";
+	
+	//for testing
+	public static final int       FILE_ID      = 14470870;
+	public static final String    FILE_TAG     = "test_tag";
+}
